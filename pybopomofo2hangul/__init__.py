8c8745522c776355cf0d31216e8b5e08f0ad88e1
LuSkywalker <callumlu@gmail.com>
2026-07-22 18:41:56 +0900

    [M] v1.0.4

--- pybopomofo2hangul/__init__.py
+++ pybopomofo2hangul/__init__.py
@@ -4,7 +4,7 @@
 from .bopomofo2hangul import bopomofo_to_hangul
 
 __title__ = 'pybopomofo2hangul'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'LuSkywalker'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2026 LuSkywalker'
