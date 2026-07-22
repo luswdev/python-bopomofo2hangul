ed989c141eb779fbc56ce9f8326f77e33215b56e
LuSkywalker <callumlu@gmail.com>
2026-07-22 18:24:16 +0900

    [M] make easier import

--- pybopomofo2hangul/__init__.py
+++ pybopomofo2hangul/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .pybopomofo2hangul import bopomofo_to_hangul
+
 __title__ = 'pybopomofo2hangul'
-__version__ = '1.0.0'
+__version__ = '1.0.3'
 __author__ = 'LuSkywalker'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2026 LuSkywalker'
